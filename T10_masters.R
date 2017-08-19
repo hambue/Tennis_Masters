1503958f9b4d88d3cf675794e88e0a67e0f7dc1b
Stephan <eubmah@gmail.com>
2017-08-19 12:02:30 +0200

    Code refactoring

--- T10_masters.R
+++ T10_masters.R
@@ -1,17 +1,24 @@
-setwd("~/R/Tennis")
+library(tidyverse)
 
-library(readr)
-library(stringr)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(purrr)
+# run only for first data import:
+#-----------------------------------------------------------
+# urls <- paste0("../data/tennis_atp/atp_matches_",
+#                1990:2017, ".csv")
+# 
+# all_csvs <- urls %>%
+#   map(read_csv) 
+# 
+# saveRDS(all_csvs, file = "data.rds")
 
-urls <- paste0("ATP/tennis_atp-master/atp_matches_",
-               1990:2015, ".csv")
+#-----------------------------------------------------------
 
-df1 <- urls %>%
-  map(read_csv) %>%
+# import all csvs
+data <- readRDS(file = "data.rds") 
+
+# find all R16 matches in Masters tournaments
+# with Top10 players involved
+# possible problem: what if no Top10 was involved?!
+df1 <- data %>%
   map(~ filter(.x, tourney_level == "M", round == "R16")) %>%
   map(~ filter(.x, winner_rank <= 10 | loser_rank <= 10)) %>%
   map(~ select(.x, tourney_name, tourney_date, round, winner_name, loser_name,
@@ -19,7 +26,10 @@ df1 <- urls %>%
   map_df(bind_rows)
   
 
-df1 %>% gather(win_los, player, winner_name, loser_name) %>%
+# new column with all player names
+# only keep rows with Top10 players
+final_df <- df1 %>% 
+  gather(win_los, player, winner_name, loser_name) %>%
   mutate(rank = ifelse(win_los == "winner_name", 
                        winner_rank, loser_rank)) %>%
   select(tourney_date, tourney_name, round, player, rank) %>%
@@ -28,9 +38,10 @@ df1 %>% gather(win_los, player, winner_name, loser_name) %>%
   summarise(number_of_T10_in_R16 = n(),
             players=paste0(player, collapse=", ")) %>%
   filter(number_of_T10_in_R16 < 5) %>%
-  arrange(number_of_T10_in_R16) -> final_df
+  arrange(number_of_T10_in_R16)
 
 
+# nice formatted table for README.md
 #knitr::kable(final_df)
 
   
